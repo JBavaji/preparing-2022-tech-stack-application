26725864aa5cb172edaf48bc7f98d599b54c7ba2
Jayeshgiri Bavaji <jbavaji@gmail.com>
2022-06-30 20:23:41 +0530

    add : forgot password ui

--- PreparationApp/app/src/main/java/com/jbavaji/preparationapp/presentation/screen/authentication/forgot_password/ForgotPasswordFragment.kt
+++ PreparationApp/app/src/main/java/com/jbavaji/preparationapp/presentation/screen/authentication/forgot_password/ForgotPasswordFragment.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.jbavaji.preparationapp.R
+import com.jbavaji.preparationapp.databinding.FragmentForgotPasswordBinding
+import com.jbavaji.preparationapp.databinding.FragmentSignInBinding
+import com.jbavaji.preparationapp.presentation.screen.authentication.signin.SignInViewModel
 
 class ForgotPasswordFragment : Fragment() {
 
@@ -14,19 +18,26 @@ class ForgotPasswordFragment : Fragment() {
         fun newInstance() = ForgotPasswordFragment()
     }
 
-    private lateinit var viewModel: ForgotPasswordViewModel
+    private lateinit var _binding: FragmentForgotPasswordBinding
+    private val binding
+        get() = _binding
+
+    private val viewModel: SignInViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+        _binding = FragmentForgotPasswordBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ForgotPasswordViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
 }
