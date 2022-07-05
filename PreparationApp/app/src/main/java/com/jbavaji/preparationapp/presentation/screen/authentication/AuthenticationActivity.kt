3d38984637f12eb4ef64c2bdd7f523192dd9864d
Jayeshgiri Bavaji <jbavaji@gmail.com>
2022-07-05 12:42:18 +0530

    add: handle title as per current fragment

--- PreparationApp/app/src/main/java/com/jbavaji/preparationapp/presentation/screen/authentication/AuthenticationActivity.kt
+++ PreparationApp/app/src/main/java/com/jbavaji/preparationapp/presentation/screen/authentication/AuthenticationActivity.kt
@@ -3,6 +3,8 @@ package com.jbavaji.preparationapp.presentation.screen.authentication
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment
 import com.jbavaji.preparationapp.R
 import com.jbavaji.preparationapp.databinding.ActivityAuthenticationBinding
@@ -24,13 +26,24 @@ class AuthenticationActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_activity_authentication) as NavHostFragment
         val navController = navHostFragment.navController
-
+        navController.addOnDestinationChangedListener(
+            listener = { _: NavController, destination: NavDestination, _: Bundle? ->
+                binding.toolbar.apply {
+                    title = when (destination.id) {
+                        R.id.signInFragment -> resources.getString(R.string.sign_in_span)
+                        else -> ""
+                    }
+                }
+            })
         binding.toolbar.apply {
-            title = ""
             setNavigationOnClickListener {
                 when (navController.currentDestination?.id) {
                     R.id.signInFragment ->
                         navController.navigate(R.id.action_signInFragment_to_signUpFragment)
+                    R.id.forgotPasswordFragment ->
+                        navController.navigate(R.id.action_forgotPasswordFragment_to_signInFragment)
+                    R.id.signUpFragment ->
+                        finish()
                 }
             }
         }
